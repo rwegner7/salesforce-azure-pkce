eaeb38e2374011f8fffd3d5bb5ebd731cb82f055
rwegner7 <ryan.wegner7@gmail.com>
2022-05-26 10:05:40 -0700

    modifications

--- force-app/main/default/lwc/azurePKCE/azurePKCE.js
+++ force-app/main/default/lwc/azurePKCE/azurePKCE.js
@@ -1,11 +1,14 @@
 import {LightningElement} from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
-import callAuthProvider from '@salesforce/apex/PKCEAzureService.callAuthProvider';
+import getCachedToken from '@salesforce/apex/PKCEAzureService.getCachedToken';
 
 export default class AzurePKCE extends NavigationMixin(LightningElement) {
 
     token; //access token returned from getCachedToken
     error; //any error that is encountered
+    device;
+    navigateTo = ''; //used for auth call to azure
+    AUTH_URL = 'https://customer-velocity-3418.cs22.my.salesforce.com/services/auth/oauth/PKCEAzure2'; //TODO put your Auth Provider OAuth-Only Initialization URL here
 
     /*--------- LIFECYCLE EVENTS ----------------------------------------------------------*/
     connectedCallback() {
@@ -23,12 +26,20 @@ export default class AzurePKCE extends NavigationMixin(LightningElement) {
 
     /*--------- AUTH CALL ----------------------------------------------------------*/
     initiateAuth(){
-        callAuthProvider()
-            .then((result) => {
+        this.navigateTo = this.AUTH_URL; //load the auth url in the iframe
+        setTimeout(() => { //wait for auth to finish (usually 2-3 seconds)
+            this.fetchCachedToken(); //get the cached token
+        },3000);
+    }
+
+    fetchCachedToken(){
+        getCachedToken() //call apex service to get the cached token
+            .then(result => {
                 this.token = result;
                 this.error = '';
                 const deviceId = JSON.parse(window.atob(result.split('.')[1])).deviceid;
                 console.log('::: device id ::: ' + deviceId);
+                this.device = deviceId;
             })
             .catch(error => {
                 this.error = error;
